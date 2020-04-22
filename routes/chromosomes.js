11835f8f310898ef8da4f0531cd65943fb0ee956
Osvaldo Fernandez <osvaldo.g.fernandez@gmail.com>
2020-04-22 00:10:18 -0300

    final touches

--- routes/chromosomes.js
+++ routes/chromosomes.js
@@ -12,28 +12,24 @@ router.get('/', function(req, res, next) {
   });
 });
 
+// ASK FOR A CHROMOSOME TO BE ASSIGNED TO MY FRONTEND
+router.get('/request', async function(req, res, next) {
+  const project = (await Project.findAll({where: {id: req.query.project_id }}))[0];
+  if (!project) {
+    next(createError(404));
+  };
+  const chromosomes = await Chromosome.findAll({where: {projectId: project.id }, include: ['palette']});
+
+  const result = _.minBy(chromosomes, 'timesRequested');
+  await result.update({ timesRequested: result.timesRequested + 1 });
+  res.send(JSON.stringify(result));
+});
+
 // GET CHROMSOME BY TRACKING ID
 router.get('/:trackingId', function(req, res, next) {
   Chromosome.findAll({ where: { trackingId: req.params.trackingId }, include: ['palette']}).then(chromosomes => {
     res.end(JSON.stringify(chromosomes[0], null, 4));
   });
-});
-
-// ASK FOR A CHROMOSOME TO BE ASSIGNED TO MY FRONTEND
-router.get('/request', async function(req, res, next) {
-  Chromosome.findAll().then(chromosomes => {
-    res.end(JSON.stringify(chromosomes, null, 4));
-  });
-
-  const project = (await Project.findAll({where: {id: req.query.project_id }, include: ['chromosomes']}))[0];
-  if (!project) {
-    next(createError(404));
-  };
-
-  const result = _.minBy(project.chromosomes, 'timesRequested');
-  console.log(project.chromosomes);
-  await result.update({ timesRequested: result.timesRequested + 1 });
-  res.send(JSON.stringify(result));
 });
 
 module.exports = router;
